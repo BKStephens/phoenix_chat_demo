8e6eb6f263f01d9e6553b9d02237f2f68ed4e9a4
Ben Stephens <BKStephens@outlook.com>
2015-12-07 21:52:17 -0800

    Changing text on landing page

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ChatDemo.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to a Phoenix Chat Demo!"
   end
 end
